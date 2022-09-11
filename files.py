deab7276ba0e3ac6140a7664ec98157daf4f48f3
Ulugbek Yusupxodjayev <ulugbek.yu@gmail.com>
2022-09-12 01:05:00 +0500

    Update

---
+++ files.py
@@ -0,0 +1,9 @@
+import os
+ 
+# Get the list of all files and directories
+path = "."
+dir_list = os.listdir(path)
+ 
+# prints all files
+for x in dir_list:
+    print("https://inwebuz.github.io/dk/" + x)
